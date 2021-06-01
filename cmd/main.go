adadff2f0245beef2ae579d8b97f0acdd8f50b3c
Рыжий Роман Леонидович <r.ryzhiy@ase-ec.ru>
2021-06-01 13:48:15 +0300

    di

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/romanlryji/todo-app"
 	"github.com/romanlryji/todo-app/pkg/handler"
+	"github.com/romanlryji/todo-app/pkg/repository"
+	"github.com/romanlryji/todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error while running http server %s\n", err.Error())
