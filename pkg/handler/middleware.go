6c7726210306e829cd25496db7827005d5a87578
Рыжий Роман Леонидович <r.ryzhiy@ase-ec.ru>
2021-06-02 16:51:12 +0300

    ParseToken

---
+++ pkg/handler/middleware.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	authHeader = "Authorization"
+	userCtx    = "userId"
+)
+
+func (h *Handler) userIdentity(c *gin.Context) {
+	header := c.GetHeader(authHeader)
+	if header == "" {
+		newErrorResponse(c, http.StatusUnauthorized, "empty auth header")
+		return
+	}
+
+	headerParts := strings.Fields(header)
+	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+		newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
+		return
+	}
+
+	if len(headerParts[1]) == 0 {
+		newErrorResponse(c, http.StatusUnauthorized, "token is empty")
+		return
+	}
+
+	userId, err := h.services.AuthorizationService.ParseToken(headerParts[1])
+	if err != nil {
+		newErrorResponse(c, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	c.Set(userCtx, userId)
+}
